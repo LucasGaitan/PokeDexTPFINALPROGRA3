cd28333dabdba585c8fb3853b0c611c818ab688f
LucasGaitan <lucasgaitan9112@gmail.com>
2022-06-02 10:41:59 -0300

    Initial commit / Project start
    -Clases de JSON creadas.
    -Entidad Pokemon.
    -Entidad Usuario.

---
+++ src/com/company/Usuario.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Usuario {
+    private int id;
+    private String userName;
+    private String password;
+    private boolean admin;
+    private Pokedex pokedex;
+    
+
+}
