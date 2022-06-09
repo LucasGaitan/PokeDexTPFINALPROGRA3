f7b59d8b1c178d396235fdff01aea70389f21d01
Julio Gularte <juliogularte15@gmail.com>
2022-06-09 08:51:58 -0300

    Agregado borrarUsuarioLista y listarUsuarios

--- src/Controladores/app/ControladoraUsuario.java
+++ src/Controladores/app/ControladoraUsuario.java
@@ -5,6 +5,8 @@ import Interfaces.app.IAbm;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 public class ControladoraUsuario implements IAbm<Usuario> {
     HashSet <Usuario> usuarios = new HashSet<Usuario>();
@@ -27,4 +29,43 @@ public class ControladoraUsuario implements IAbm<Usuario> {
     public HashSet<Usuario> listar(){
         return null;
     }
+
+    String borrarUsuarioLista (Usuario u) //testear
+    {
+        boolean found=false;
+        Iterator i=usuarios.iterator();
+        Usuario aBorrar=new Usuario();
+        String response=new String();
+        while (i.hasNext() && !found)
+        {
+            if ((Usuario)i.next()==u)
+            {
+                aBorrar=(Usuario)i.next();
+                found=true;
+            }
+        }
+        if (usuarios.contains(aBorrar))
+        {
+            response=u.getUserName();
+            usuarios.remove(aBorrar);
+
+        }
+        else
+        {
+            ///throw usuarionotfoundexception
+        }
+        return response;
+    }
+
+    public List <String> listarUsuarios () //test
+    {
+        List<String> lista=new ArrayList<String>();
+        for (Usuario u:usuarios)
+        {
+            Usuario usuario=new Usuario();
+            usuario.setUserName(u.getUserName());
+        }
+        return lista;
+    }
+
 }
--- src/company/app/Main.java
+++ src/company/app/Main.java
@@ -2,17 +2,15 @@ package company.app;
 
 
 
-import Controladores.app.ControladoraJSON;
-import Entidad.app.Pokemon;
-
-import java.util.LinkedHashSet;
-
 public class Main {
 
     public static void main(String[] args) {
+        /*
         ControladoraJSON controladoraJSON = new ControladoraJSON("Pokemons");
         LinkedHashSet<Pokemon> pokemons = new LinkedHashSet<>();
         pokemons = controladoraJSON.generarPokemon();
         System.out.println(pokemons);
+
+         */
     }
 }
