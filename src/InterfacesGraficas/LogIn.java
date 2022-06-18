7a1aa5dff5ca44168216e3be2428b5615b6d58ae
Julio Gularte <juliogularte15@gmail.com>
2022-06-18 03:12:50 -0300

    Agregada ventana principal admin

--- src/InterfacesGraficas/LogIn.java
+++ src/InterfacesGraficas/LogIn.java
@@ -1,16 +1,20 @@
 package InterfacesGraficas;
+
 import company.app.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
+
 public class LogIn {
 
     public LogIn() {
@@ -27,7 +31,6 @@ public class LogIn {
     private PasswordField password;
 
 
-
     public void userLogIn(ActionEvent event) throws IOException {
         checkLogin();
 
@@ -35,16 +38,22 @@ public class LogIn {
 
     private void checkLogin() throws IOException {
         Main m = new Main();
-        if(username.getText().toString().equals("prueba") && password.getText().toString().equals("123")) {
+
+        if (username.getText().toString().equals("prueba") && password.getText().toString().equals("123")) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("PrincipalAdmin.fxml"));
             wrongLogIn.setText("pokimon");
-        }
+            Scene scene = new Scene(root);
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("New Window");
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.NONE);// default
+            primaryStage.show();
 
-        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
+
+        } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
             wrongLogIn.setText("Por favor, ingrese sus datos");
-        }
-
-
-        else {
+        } else {
             wrongLogIn.setText("Usuario o contraseña incorrectos!");
         }
     }
