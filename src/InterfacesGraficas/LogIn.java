57c6b372e2fcef618b8fe692d11124e8370f3263
Julio Gularte <juliogularte15@gmail.com>
2022-06-21 12:38:06 -0300

    Revert "Arreglado error scene"
    This reverts commit b8cba24b62e92d81c1cd34479749d55f052a032b.

--- src/InterfacesGraficas/LogIn.java
+++ src/InterfacesGraficas/LogIn.java
@@ -2,19 +2,15 @@ package InterfacesGraficas;
 
 import company.app.Main;
 import javafx.fxml.FXML;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
@@ -38,37 +34,29 @@ public class LogIn {
     public void userLogIn(ActionEvent event) throws IOException {
         checkLogin();
 
-        PrincipalAdmin controller = new PrincipalAdmin();
-        controller.administracion(event);
-
-
     }
 
     private void checkLogin() throws IOException {
+        Main m = new Main();
 
         if (username.getText().toString().equals("prueba") && password.getText().toString().equals("123")) {
 
-
+            Parent root = FXMLLoader.load(getClass().getResource("PrincipalAdmin.fxml"));
+            wrongLogIn.setText("pokimon");
+            Scene scene = new Scene(root);
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("New Window");
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.NONE);// default
+            primaryStage.show();
 
 
         } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
-
-            //ControladoraUsuario controladoraUsuario = new ControladoraUsuario();
-        /*
-        try {
-            Usuario encontrado = controladoraUsuario.login(this.username.getText(),this.password.getText());
-            m.changeScene("InterfacesGraficas/User.fxml");
-        } catch (EUsuarioPassIncorrecta e) {
-            wrongLogIn.setText("Usuario o contraseña incorrectos!");
-        } catch (EUsuarioNotFound e) {
             wrongLogIn.setText("Por favor, ingrese sus datos");
         } else {
             wrongLogIn.setText("Usuario o contraseña incorrectos!");
         }
-        */
-
-        }
-
-
     }
+
+
 }
